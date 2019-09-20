234447b49334c7fbd34951ce2b011e3b57144692
MrAxelsen <joax@live.dk>
2019-09-20 10:59:01 +0200

    finished uvc_capture.py for testing

--- cam_server.py
+++ cam_server.py
@@ -6,12 +6,12 @@ PORT = 65433        # Port to listen on (non-privileged ports are > 1023)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind((HOST, PORT))
 	s.listen()
+	s.setblocking(0)
 	conn, addr = s.accept()
 	with conn:
 		print('Connected by', addr)
 		while True:
-			data = c
-			onn.recv(1024).decode()
+			data = conn.recv(1024).decode()
 			if data == 'request_angle':
 				answer = '-17:22'
 				print(str(data))
--- uvc_capture.py
+++ uvc_capture.py
@@ -1,42 +1,154 @@
-from __future__ import print_function
-import ./uvc/uvc
-import logging
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-logging.basicConfig(level=logging.INFO)
-
-dev_list = uvc.device_list()
-print(dev_list)
-cap = uvc.Capture(dev_list[0]["uid"])
-
-# Uncomment the following lines to configure the Pupil 200Hz IR cameras:
-# controls_dict = dict([(c.display_name, c) for c in cap.controls])
-# controls_dict['Auto Exposure Mode'].value = 1
-# controls_dict['Gamma'].value = 200
-
-print(cap.frame_mode)
-for x in range(10):
-    print(x)
-    cap.frame_mode = (640, 480, 30)
-    for x in range(100):
-        frame = cap.get_frame_robust()
-        print(frame.img.shape)
-        # cv2.imshow("img",frame.gray)
-        # cv2.waitKey(1)
-cap = None
-
-
-
-'''
 from uvctypes import *
 import time
 import cv2
 import numpy as np
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65433        # Port to listen on (non-privileged ports are > 1023)
+
+try:
+  from queue import Queue
+except ImportError:
+  from Queue import Queue
+import platform
+
+BUF_SIZE = 2
+q = Queue(BUF_SIZE)
+
+def py_frame_callback(frame, userptr):
+
+  array_pointer = cast(frame.contents.data, POINTER(c_uint16 * (frame.contents.width * frame.contents.height)))
+  data = np.frombuffer(
+    array_pointer.contents, dtype=np.dtype(np.uint16)
+  ).reshape(
+    frame.contents.height, frame.contents.width
+  ) # no copy
+
+  # data = np.fromiter(
+  #   frame.contents.data, dtype=np.dtype(np.uint8), count=frame.contents.data_bytes
+  # ).reshape(
+  #   frame.contents.height, frame.contents.width, 2
+  # ) # copy
+
+  if frame.contents.data_bytes != (2 * frame.contents.width * frame.contents.height):
+    return
+
+  if not q.full():
+    q.put(data)
+
+PTR_PY_FRAME_CALLBACK = CFUNCTYPE(None, POINTER(uvc_frame), c_void_p)(py_frame_callback)
+
+def ktof(val):
+  return (1.8 * ktoc(val) + 32.0)
+
+def ktoc(val):
+  return (val - 27315) / 100.0
+
+def raw_to_8bit(data):
+  cv2.normalize(data, data, 0, 65535, cv2.NORM_MINMAX)
+  np.right_shift(data, 8, data)
+  return cv2.cvtColor(np.uint8(data), cv2.COLOR_GRAY2RGB)
 
 def display_temperature(img, val_k, loc, color):
-  val = ktof(val_k)
-  cv2.putText(img,"{0:.1f} degF".format(val), loc, cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
+  val = ktoc(val_k)
+  cv2.putText(img,"{0:.1f} degC".format(val), loc, cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
   x, y = loc
   cv2.line(img, (x - 2, y), (x + 2, y), color, 1)
   cv2.line(img, (x, y - 2), (x, y + 2), color, 1)
-  
-'''
+
+def main(conn):
+  ctx = POINTER(uvc_context)()
+  dev = POINTER(uvc_device)()
+  devh = POINTER(uvc_device_handle)()
+  ctrl = uvc_stream_ctrl()
+
+  res = libuvc.uvc_init(byref(ctx), 0)
+  if res < 0:
+    print("uvc_init error")
+    exit(1)
+
+  try:
+    res = libuvc.uvc_find_device(ctx, byref(dev), PT_USB_VID, PT_USB_PID, 0)
+    if res < 0:
+      print("uvc_find_device error")
+      exit(1)
+
+    try:
+      res = libuvc.uvc_open(dev, byref(devh))
+      if res < 0:
+        print("uvc_open error")
+        exit(1)
+
+      print("device opened!")
+
+      print_device_info(devh)
+      print_device_formats(devh)
+
+      frame_formats = uvc_get_frame_formats_by_guid(devh, VS_FMT_GUID_Y16)
+      if len(frame_formats) == 0:
+        print("device does not support Y16")
+        exit(1)
+
+      libuvc.uvc_get_stream_ctrl_format_size(devh, byref(ctrl), UVC_FRAME_FORMAT_Y16,
+        frame_formats[0].wWidth, frame_formats[0].wHeight, int(1e7 / frame_formats[0].dwDefaultFrameInterval)
+      )
+
+      res = libuvc.uvc_start_streaming(devh, byref(ctrl), PTR_PY_FRAME_CALLBACK, None, 0)
+      if res < 0:
+        print("uvc_start_streaming failed: {0}".format(res))
+        exit(1)
+
+      try:
+        while True:
+          data = q.get(True, 500)
+          if data is None:
+            break
+          data = cv2.resize(data[:,:], (640, 480))
+          minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(data)
+          maxV = ktoc(maxVal)
+          dgPpx = 57/160
+          maxX = (maxLoc[0]-(160/2))*dgPpx
+          maxY = (maxLoc[1]-(120/2))*dgPpx
+          try:
+            data = conn.recv(1024).decode()
+            if data == 'request_angle':
+              if maxV >= 50:
+                answer = str(maxX) + ',' + str(maxY)
+              elif maxV < 50:
+                answer = 'no_fire'
+            elif data == 'shutdown':
+              break
+            conn.sendall(answer.encode())
+          except BlockingIOError:
+            pass
+          '''
+          img = raw_to_8bit(data)
+          display_temperature(img, maxVal, maxLoc, (0, 0, 255))
+          cv2.imshow('Lepton Radiometry', img)
+          cv2.waitKey(1)
+
+        cv2.destroyAllWindows()
+        '''
+      finally:
+        libuvc.uvc_stop_streaming(devh)
+
+      print("done")
+    finally:
+      libuvc.uvc_unref_device(dev)
+  finally:
+    libuvc.uvc_exit(ctx)
+
+if __name__ == '__main__':
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    s.setblocking(0)
+    with conn:
+      print('Connected by', addr)
+      while True:
+        main(conn)
